85d94d4e1d3de16f2ce73b0181808c033f4381e9
Mark Shroyer <code@markshroyer.com>
2010-12-25 16:57:57 -0500

    Convert tabs to spaces
    Replaced all tabs with equivalent space indentation (assuming 4 spaces per
    tab stop).

--- ImageResource.h
+++ ImageResource.h
@@ -4,12 +4,12 @@
 class ImageResource
 {
 public:
-	ImageResource(LPCTSTR lpName, HMODULE hInst = NULL, LPCTSTR lpType = MAKEINTRESOURCE(RT_RCDATA));
-	~ImageResource(void);
-	Gdiplus::Image* lpImage;
+    ImageResource(LPCTSTR lpName, HMODULE hInst = NULL, LPCTSTR lpType = MAKEINTRESOURCE(RT_RCDATA));
+    ~ImageResource(void);
+    Gdiplus::Image* lpImage;
 
 private:
-	HGLOBAL hBuffer;
-	void* lpBuffer;
-	bool Load(LPCTSTR lpName, HMODULE hInst, LPCTSTR lpType);
+    HGLOBAL hBuffer;
+    void* lpBuffer;
+    bool Load(LPCTSTR lpName, HMODULE hInst, LPCTSTR lpType);
 };
--- MagicWord.cpp
+++ MagicWord.cpp
@@ -8,18 +8,18 @@
 #define TIMER_ARM 1
 
 // Global Variables:
-HINSTANCE hInst;								// current instance
-TCHAR szTitle[MAX_LOADSTRING];					// The title bar text
-TCHAR szWindowClass[MAX_LOADSTRING];			// the main window class name
+HINSTANCE hInst;                                // current instance
+TCHAR szTitle[MAX_LOADSTRING];                  // The title bar text
+TCHAR szWindowClass[MAX_LOADSTRING];            // the main window class name
 ImageResource* nedryBody;
 ImageResource* nedryFace;
 ImageResource* nedryArm;
 
 // Forward declarations:
-ATOM				MyRegisterClass(HINSTANCE hInstance);
-BOOL				InitInstance(HINSTANCE, int);
-LRESULT CALLBACK	WndProc(HWND, UINT, WPARAM, LPARAM);
-INT_PTR CALLBACK	About(HWND, UINT, WPARAM, LPARAM);
+ATOM                MyRegisterClass(HINSTANCE hInstance);
+BOOL                InitInstance(HINSTANCE, int);
+LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
+INT_PTR CALLBACK    About(HWND, UINT, WPARAM, LPARAM);
 void ResizeClient(HWND, int, int);
 void Draw(HDC, int, int);
 
@@ -28,62 +28,62 @@ int APIENTRY _tWinMain(HINSTANCE hInstance,
                      LPTSTR    lpCmdLine,
                      int       nCmdShow)
 {
-	UNREFERENCED_PARAMETER(hPrevInstance);
-	UNREFERENCED_PARAMETER(lpCmdLine);
+    UNREFERENCED_PARAMETER(hPrevInstance);
+    UNREFERENCED_PARAMETER(lpCmdLine);
 
-	MSG msg;
-	HACCEL hAccelTable;
-	Gdiplus::GdiplusStartupInput gdiplusStartupInput;
-	ULONG_PTR gdiplusToken;
+    MSG msg;
+    HACCEL hAccelTable;
+    Gdiplus::GdiplusStartupInput gdiplusStartupInput;
+    ULONG_PTR gdiplusToken;
 
-	// We're using GDI+ to make things easier (and for higher quality)...
-	Gdiplus::GdiplusStartup(&gdiplusToken, &gdiplusStartupInput, NULL);
+    // We're using GDI+ to make things easier (and for higher quality)...
+    Gdiplus::GdiplusStartup(&gdiplusToken, &gdiplusStartupInput, NULL);
 
-	// Initialize global strings
-	LoadString(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
-	LoadString(hInstance, IDC_MAGICWORD, szWindowClass, MAX_LOADSTRING);
+    // Initialize global strings
+    LoadString(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
+    LoadString(hInstance, IDC_MAGICWORD, szWindowClass, MAX_LOADSTRING);
 
-	MyRegisterClass(hInstance);
+    MyRegisterClass(hInstance);
 
-	// Perform application initialization:
-	if (!InitInstance (hInstance, nCmdShow))
-	{
-		return FALSE;
-	}
+    // Perform application initialization:
+    if (!InitInstance (hInstance, nCmdShow))
+    {
+        return FALSE;
+    }
 
-	hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_MAGICWORD));
-	while (GetMessage(&msg, NULL, 0, 0))
-	{
-		if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg))
-		{
-			TranslateMessage(&msg);
-			DispatchMessage(&msg);
-		}
-	}
+    hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_MAGICWORD));
+    while (GetMessage(&msg, NULL, 0, 0))
+    {
+        if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg))
+        {
+            TranslateMessage(&msg);
+            DispatchMessage(&msg);
+        }
+    }
 
-	Gdiplus::GdiplusShutdown(gdiplusToken);
-	return (int) msg.wParam;
+    Gdiplus::GdiplusShutdown(gdiplusToken);
+    return (int) msg.wParam;
 }
 
 
 ATOM MyRegisterClass(HINSTANCE hInstance)
 {
-	WNDCLASSEX wcex;
+    WNDCLASSEX wcex;
 
-	wcex.cbSize			= sizeof(WNDCLASSEX);
-	wcex.style			= CS_HREDRAW | CS_VREDRAW;
-	wcex.lpfnWndProc	= WndProc;
-	wcex.cbClsExtra		= 0;
-	wcex.cbWndExtra		= 0;
-	wcex.hInstance		= hInstance;
-	wcex.hIcon			= LoadIcon(hInstance, MAKEINTRESOURCE(IDI_MAGICWORD));
-	wcex.hCursor		= LoadCursor(NULL, IDC_ARROW);
-	wcex.hbrBackground	= (HBRUSH)(COLOR_WINDOW+1);
-	wcex.lpszMenuName	= MAKEINTRESOURCE(IDC_MAGICWORD);
-	wcex.lpszClassName	= szWindowClass;
-	wcex.hIconSm		= LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_MAGICWORD));
+    wcex.cbSize         = sizeof(WNDCLASSEX);
+    wcex.style          = CS_HREDRAW | CS_VREDRAW;
+    wcex.lpfnWndProc    = WndProc;
+    wcex.cbClsExtra     = 0;
+    wcex.cbWndExtra     = 0;
+    wcex.hInstance      = hInstance;
+    wcex.hIcon          = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_MAGICWORD));
+    wcex.hCursor        = LoadCursor(NULL, IDC_ARROW);
+    wcex.hbrBackground  = (HBRUSH)(COLOR_WINDOW+1);
+    wcex.lpszMenuName   = MAKEINTRESOURCE(IDC_MAGICWORD);
+    wcex.lpszClassName  = szWindowClass;
+    wcex.hIconSm        = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_MAGICWORD));
 
-	return RegisterClassEx(&wcex);
+    return RegisterClassEx(&wcex);
 }
 
 
@@ -107,154 +107,154 @@ BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
 
 LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 {
-	int wmId, wmEvent;
-	PAINTSTRUCT ps;
-	HDC hdc;
-	HDC hdcBuffer;
-	HBITMAP hbmpBuffer, hbmpOld;
-	static int cxClient, cyClient;
-	static int cxSource, cySource;
+    int wmId, wmEvent;
+    PAINTSTRUCT ps;
+    HDC hdc;
+    HDC hdcBuffer;
+    HBITMAP hbmpBuffer, hbmpOld;
+    static int cxClient, cyClient;
+    static int cxSource, cySource;
 
-	switch (message)
-	{
-	case WM_COMMAND:
-		wmId = LOWORD(wParam);
-		wmEvent = HIWORD(wParam);
-		switch (wmId)
-		{
-		case IDM_ABOUT:
-			DialogBox(hInst, MAKEINTRESOURCE(IDD_ABOUTBOX), hWnd, About);
-			break;
-		case IDM_EXIT:
-			DestroyWindow(hWnd);
-			break;
-		default:
-			return DefWindowProc(hWnd, message, wParam, lParam);
-		}
-		break;
+    switch (message)
+    {
+    case WM_COMMAND:
+        wmId = LOWORD(wParam);
+        wmEvent = HIWORD(wParam);
+        switch (wmId)
+        {
+        case IDM_ABOUT:
+            DialogBox(hInst, MAKEINTRESOURCE(IDD_ABOUTBOX), hWnd, About);
+            break;
+        case IDM_EXIT:
+            DestroyWindow(hWnd);
+            break;
+        default:
+            return DefWindowProc(hWnd, message, wParam, lParam);
+        }
+        break;
 
-	case WM_CREATE:
-		nedryBody = new ImageResource(MAKEINTRESOURCE(IDR_NEDRY_BODY));
-		nedryFace = new ImageResource(MAKEINTRESOURCE(IDR_NEDRY_FACE));
-		nedryArm  = new ImageResource(MAKEINTRESOURCE(IDR_NEDRY_ARM));
+    case WM_CREATE:
+        nedryBody = new ImageResource(MAKEINTRESOURCE(IDR_NEDRY_BODY));
+        nedryFace = new ImageResource(MAKEINTRESOURCE(IDR_NEDRY_FACE));
+        nedryArm  = new ImageResource(MAKEINTRESOURCE(IDR_NEDRY_ARM));
 
-		SetTimer(hWnd, TIMER_ARM, 33, NULL);
+        SetTimer(hWnd, TIMER_ARM, 33, NULL);
 
-		PlaySound(MAKEINTRESOURCE(IDR_MAGICWRD), hInst, SND_RESOURCE | SND_ASYNC | SND_LOOP);
+        PlaySound(MAKEINTRESOURCE(IDR_MAGICWRD), hInst, SND_RESOURCE | SND_ASYNC | SND_LOOP);
 
-		break;
+        break;
 
-	case WM_SIZE:
-		cxClient = LOWORD(lParam);
-		cyClient = HIWORD(lParam);
-		break;
+    case WM_SIZE:
+        cxClient = LOWORD(lParam);
+        cyClient = HIWORD(lParam);
+        break;
 
-	case WM_TIMER:
-		switch (wParam) {
-		case TIMER_ARM:
-			InvalidateRgn(hWnd, NULL, FALSE);
-			UpdateWindow(hWnd);
-			break;
-		}
-		break;
+    case WM_TIMER:
+        switch (wParam) {
+        case TIMER_ARM:
+            InvalidateRgn(hWnd, NULL, FALSE);
+            UpdateWindow(hWnd);
+            break;
+        }
+        break;
 
-	case WM_PAINT:
-		hdc = BeginPaint(hWnd, &ps);
+    case WM_PAINT:
+        hdc = BeginPaint(hWnd, &ps);
 
-		// Double buffering to present animation flicker
-		hdcBuffer = CreateCompatibleDC(hdc);
-		hbmpBuffer = CreateCompatibleBitmap(hdc, cxClient, cyClient);
-		hbmpOld = (HBITMAP) SelectObject(hdcBuffer, hbmpBuffer);
+        // Double buffering to present animation flicker
+        hdcBuffer = CreateCompatibleDC(hdc);
+        hbmpBuffer = CreateCompatibleBitmap(hdc, cxClient, cyClient);
+        hbmpOld = (HBITMAP) SelectObject(hdcBuffer, hbmpBuffer);
 
-		Draw(hdcBuffer, cxClient, cyClient);
-		BitBlt(hdc, 0, 0, cxClient, cyClient, hdcBuffer, 0, 0, SRCCOPY);
+        Draw(hdcBuffer, cxClient, cyClient);
+        BitBlt(hdc, 0, 0, cxClient, cyClient, hdcBuffer, 0, 0, SRCCOPY);
 
-		SelectObject(hdcBuffer, hbmpOld);
-		DeleteObject(hbmpBuffer);
-		DeleteDC(hdcBuffer);
+        SelectObject(hdcBuffer, hbmpOld);
+        DeleteObject(hbmpBuffer);
+        DeleteDC(hdcBuffer);
 
-		EndPaint(hWnd, &ps);
-		break;
+        EndPaint(hWnd, &ps);
+        break;
 
-	case WM_DESTROY:
-		KillTimer(hWnd, TIMER_ARM);
-		delete nedryBody;
-		delete nedryFace;
-		delete nedryArm;
-		PostQuitMessage(0);
-		break;
+    case WM_DESTROY:
+        KillTimer(hWnd, TIMER_ARM);
+        delete nedryBody;
+        delete nedryFace;
+        delete nedryArm;
+        PostQuitMessage(0);
+        break;
 
-	default:
-		return DefWindowProc(hWnd, message, wParam, lParam);
-	}
-	return 0;
+    default:
+        return DefWindowProc(hWnd, message, wParam, lParam);
+    }
+    return 0;
 }
 
 // Message handler for about box.
 INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
 {
-	UNREFERENCED_PARAMETER(lParam);
-	switch (message)
-	{
-	case WM_INITDIALOG:
-		return (INT_PTR)TRUE;
+    UNREFERENCED_PARAMETER(lParam);
+    switch (message)
+    {
+    case WM_INITDIALOG:
+        return (INT_PTR)TRUE;
 
-	case WM_COMMAND:
-		if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL)
-		{
-			EndDialog(hDlg, LOWORD(wParam));
-			return (INT_PTR)TRUE;
-		}
-		break;
-	}
-	return (INT_PTR)FALSE;
+    case WM_COMMAND:
+        if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL)
+        {
+            EndDialog(hDlg, LOWORD(wParam));
+            return (INT_PTR)TRUE;
+        }
+        break;
+    }
+    return (INT_PTR)FALSE;
 }
 
 // Resize the window to result in a client area of exactly the specified dimensions
 void ResizeClient(HWND hWnd, int width, int height)
 {
-	RECT rectClient, rectWindow;
-	POINT ptDiff;
-	GetClientRect(hWnd, &rectClient);
-	GetWindowRect(hWnd, &rectWindow);
-	ptDiff.x = width - rectClient.right;
-	ptDiff.y = height - rectClient.bottom;
+    RECT rectClient, rectWindow;
+    POINT ptDiff;
+    GetClientRect(hWnd, &rectClient);
+    GetWindowRect(hWnd, &rectWindow);
+    ptDiff.x = width - rectClient.right;
+    ptDiff.y = height - rectClient.bottom;
 
-	MoveWindow(hWnd,
-				rectWindow.left,
-				rectWindow.top,
-				(rectWindow.right - rectWindow.left) + ptDiff.x,
-				(rectWindow.bottom - rectWindow.top) + ptDiff.y,
-				TRUE);
+    MoveWindow(hWnd,
+                rectWindow.left,
+                rectWindow.top,
+                (rectWindow.right - rectWindow.left) + ptDiff.x,
+                (rectWindow.bottom - rectWindow.top) + ptDiff.y,
+                TRUE);
 }
 
 // Draw Dennis Nedry's winsome mug
 void Draw(HDC hdc, int width, int height)
 {
-	FILETIME ft;
-	GetSystemTimeAsFileTime(&ft);
+    FILETIME ft;
+    GetSystemTimeAsFileTime(&ft);
 
-	ULONGLONG ftime = ( ( (ULONGLONG) ft.dwHighDateTime) << 32 ) + ft.dwLowDateTime;
-	static ULONGLONG ftimeInit = ftime;
-	ULONGLONG ftimeDelta = ftime - ftimeInit;
+    ULONGLONG ftime = ( ( (ULONGLONG) ft.dwHighDateTime) << 32 ) + ft.dwLowDateTime;
+    static ULONGLONG ftimeInit = ftime;
+    ULONGLONG ftimeDelta = ftime - ftimeInit;
 
-	Gdiplus::Matrix affineTransform;
-	Gdiplus::Status result;
+    Gdiplus::Matrix affineTransform;
+    Gdiplus::Status result;
 
-	Gdiplus::Graphics graphics(hdc);
-	graphics.Clear(Gdiplus::Color(0xff, 0xff, 0xff, 0xff));
+    Gdiplus::Graphics graphics(hdc);
+    graphics.Clear(Gdiplus::Color(0xff, 0xff, 0xff, 0xff));
 
-	Gdiplus::Point ptNedryBody(width / 2 - 100, height / 2 - 60);
-	graphics.DrawImage(nedryBody->lpImage, ptNedryBody);
+    Gdiplus::Point ptNedryBody(width / 2 - 100, height / 2 - 60);
+    graphics.DrawImage(nedryBody->lpImage, ptNedryBody);
 
-	Gdiplus::Point ptNedryFace(width / 2 - 54, height / 2 - 180);
-	graphics.DrawImage(nedryFace->lpImage, ptNedryFace);
+    Gdiplus::Point ptNedryFace(width / 2 - 54, height / 2 - 180);
+    graphics.DrawImage(nedryFace->lpImage, ptNedryFace);
 
-	double angle = 15 * cos( (double) ( ( ftimeDelta >> 14 ) % 360 ) * M_PI / 180 );
-	Gdiplus::PointF ptNedryArm(width / 2 + 50, height / 2 - 100);
-	Gdiplus::PointF ptNedryArmAxisOffset(13, 108);
-	affineTransform.RotateAt((int) angle, ptNedryArm + ptNedryArmAxisOffset);
-	graphics.SetTransform(&affineTransform);
-	graphics.DrawImage(nedryArm->lpImage, ptNedryArm);
-	graphics.ResetTransform();
+    double angle = 15 * cos( (double) ( ( ftimeDelta >> 14 ) % 360 ) * M_PI / 180 );
+    Gdiplus::PointF ptNedryArm(width / 2 + 50, height / 2 - 100);
+    Gdiplus::PointF ptNedryArmAxisOffset(13, 108);
+    affineTransform.RotateAt((int) angle, ptNedryArm + ptNedryArmAxisOffset);
+    graphics.SetTransform(&affineTransform);
+    graphics.DrawImage(nedryArm->lpImage, ptNedryArm);
+    graphics.ResetTransform();
 }
